d6a70d8c62fca62871ff3331d5611ddca1a73998
Katy Blumer <katherine.blumer@gmail.com>
2020-02-09 15:34:10 -0500

    Add RWR functions (not tested yet)

--- R/BadBootstrapping.R
+++ R/BadBootstrapping.R
@@ -1,6 +1,7 @@
 #Different types of bootstrapping for paper
 
-paperValidator<-Relevant_QTL[,c(1,2,3,5,4,6)]
+# paperValidator<-Relevant_QTL[,c(1,2,3,5,4,6)]
+Relevant_QTL<-unique(read.csv("FakeQTL",stringsAsFactors = F)
 colnames(paperValidator)[1]<-'chromosome'
 
 BootStrapConfidenceIntervals<-as.data.frame(matrix(nrow=24,ncol=length(Relevant_QTL$ChrNum)))
@@ -1758,19 +1759,143 @@ for (QTL in 1:length(paperValidator$chromosome)){
   BootStrapConfidenceIntervals[d,QTL]<-quantile(BS_dist,toQuantile)
 }
 
+# RWR ######
+
+sampleUniformQTLLoc <- function(skip_hangovers, bidirectional, qtl_length, chromosome_size) {
+  allowed_chr <- chromosome_size[which(chromosome_size$Length >= qtl_length), ]
+  sampled_chr <- allowed_chr[sample(nrow(allowed_chr), 1, prob = allowed_chr$Length), ]
+  chr_length <- sampled_chr$Length
+
+  if (skip_hangovers) {
+    start_base <- sample(chr_length - qtl_length, 1)
+  } else {  # bounceback
+    start_base <- sample(chr_length, 1)
+    if (start_base + qtl_length > chr_length) {
+      start_base <- chr_length - qtl_length
+    }
+  }
+
+  qtl_range <- c(start_base, start_base + qtl_length)
+
+  if (bidirectional) {
+    # 50% chance to start range from opposite end
+    if (sample(c(TRUE, FALSE), 1)) {
+      qtl_range <- c(chr_length - (start_base + qtl_length) + 1, chr_length - start_base + 1)
+    }
+  }
+  return(list(sampled_chr, qtl_range))
+}
+
+getAllowedMarkers <- function(
+  skip_hangovers,
+  qtl_length,
+  marker_list
+) {
+  for (chr in unique(marker_list$Chromosome)) {
+    chr_indices <- which(marker_list$Chromosome == chr)
+    last_avail_base <- max(marker_list[chr_indices, "Base"]) - qtl_length
+
+    # Either remove disallowed markers or do bounceback
+    unavail_equiv_base <- if (skip_hangovers) 0 else last_avail_base
+    marker_list[chr_indices & marker_list$Base > last_avail_base, "Base"] <- unavail_equiv_base
+  }
+
+  marker_list <- marker_list[which(marker_list$Base > 0)]
+  return(marker_list)
+}
+
+RWR <- function(
+  markers_only,  # vs. random placement
+  skip_hangovers,  # vs bounceback
+  bidirectional,  # vs left-to-right only
+  drop_tandem,
+  qtl_list,
+  gene_list,
+  marker_list,
+  chromosome_size
+) {
+
+  gene_list$Trait <- "trait"
+  if (drop_tandem) {
+    gene_list <- FilterGeneList("trait", "extension", gene_list, marker_list)
+  }
+
+  qtl_cis <- rep(0, nrow(qtl_list))
+
+  for (qtl_i in nrow(qtl_list)) {
+    qtl_length <- qtl_list[qtl_i, "Length"]
+    obs_genes_found <- qtl_list[qtl_i, "NumGenes"]  # TODO have to calc this?
+    exp_genes_found <- rep(0, n_reps)
+
+    if (markers_only) {
+      allowed_markers <- getAllowedMarkers(skip_hangovers, qtl_length, marker_list)
+      allowed_markers <- cbind(allowed_markers, is_l_to_r=TRUE)
+
+      if (bidirectional) {
+        rl_markers <- cbind(marker_list, is_l_to_r=FALSE)
+
+        chr_lengths <- merge(rl_markers, chromosome_size[, c("Number", "Length")],
+                            by.x="Chromosome", by.y="Number")
+        rl_markers$Base <- chr_lengths$Length - rl_markers$Base + 1  # reverse bases
+        rl_markers <- getAllowedMarkers(skip_hangovers, qtl_length, rl_markers)
+        rl_markers$Base <- chr_lengths$Length - rl_markers$Base + 1  # reverse them back
+
+        allowed_markers <- rbind(allowed_markers, rl_markers)
+      }
+    }
+
+    for (rep_i in n_reps) {
+      if (markers_only) {
+        sampled_marker <- allowed_markers[sample(nrow(allowed_markers, 1)), ]
+        sampled_chr <- sampled_marker$Chromosome
+
+        if (sampled_marker$is_r_to_l) {
+          qtl_range <- c(sampled_marker$Base, sampled_marker$Base + qtl_length)
+        } else {
+          qtl_range <- c(sampled_marker$Base - qtl_length, sampled_marker$Base)
+        }
+
+      } else {  # !markers_only (all bases)
+        chr_and_range <- sampleUniformQTLLoc(skip_hangovers, bidirectional, qtl_length, chromosome_size)
+        sampled_chr <- chr_and_range[[1]]
+        qtl_range <- chr_and_range[[2]]
+      }
+
+      genes_found = nrow(target_gene_list[
+          which(target_gene_list$Chromosome == sampled_chr) &
+          which(target_gene_list$Base >= qtl_range[1]) &
+          which(target_gene_list$Base <= qtl_range[2])
+        ,])
+      exp_genes_found[rep_i] <- genes_found
+
+    }  # endfor n_reps
+
+    # no touchy the math
+    BS_dist <- exp_genes_found
+    GHat <- ecdf(BS_dist)
+    z0 <- qnorm(GHat(obs_genes_found))
+
+    dSubI <- bootstrap::jackknife(as.vector(BS_dist),complex_Fun)
+    accelConstant <- (1/6) * sum(dSubI$jack.values ^ 3) / ((sum(dSubI$jack.values ^ 2)) ^ (2/3))
+
+    bcaNum <- z0 + 1.96
+    bcaDen <- 1 - accelConstant*(z0+1.96)
+    if (z0 == Inf) {
+      toQuantile <- NA
+    } else {
+      allIn <- z0 + bcaNum / bcaDen
+      toQuantile <- pnorm(allIn)
+    }
+
+    qtl_cis[qtl_i] <- quantile(BS_dist, toQuantile)
+  } # endfor qtl_list
+  return(qtl_cis)
+}
+
 # ?? #####
 ceiling(BootStrapConfidenceIntervals)
 colnames(BootStrapConfidenceIntervals)<-Relevant_QTL$Length
 
-RWR <- function(
-  markers_only, # bool (vs. random placement)
-  no_bounceback, # bool
-  cutoff_beyond_end, # bool (was this a paste error - isnt it bounceback OR cutoff OR skip?)
-  bidirectional, # bool
-  drop_duplicates # bool
-) {
-  temp -> "TEMP"
-}
 
 #Singles Heat Map
 
