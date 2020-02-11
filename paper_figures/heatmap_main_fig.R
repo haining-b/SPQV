ea9b82151a33467814e1bd3b717e2872fc9bb7ba
Katy Blumer <katherine.blumer@gmail.com>
2020-02-11 01:25:38 -0500

    Add ceiling plot

--- paper_figures/heatmap_main_fig.R
+++ paper_figures/heatmap_main_fig.R
@@ -41,6 +41,8 @@ regenerate_results <- TRUE
 
 # Run RWR and SPQV ####
 if (regenerate_results) {
+  sim_results_env <- new.env()
+
   RWR_results <- as.data.frame(matrix(nrow=16,ncol=nrow(qtl_list)))
 
   row_i <- 1
@@ -61,14 +63,15 @@ if (regenerate_results) {
           gene_list = gene_list,
           marker_list = marker_list,
           chromosome_size = chromosome_size,
-          n_reps = num_reps
+          n_reps = num_reps,
+          intermediate_output_env = sim_results_env
         )
         row_i <- row_i + 1
       }
     }
   }
 
-  write.csv(x = RWR_results, file = "paper_figures/data/RWR_results.csv", row.names=FALSE)
+  write.csv(x = RWR_results, file = "paper_figures/output_data/RWR_results.csv", row.names=FALSE)
 
 
 
@@ -81,10 +84,10 @@ if (regenerate_results) {
     marker_list = marker_list,
     wgd,
     chromosome_size = chromosome_size,
-    new.env()
+    sim_results_env
   )
 
-  write.csv(x = SPQV_results, file = "paper_figures/data/SPQV_results.csv", row.names=FALSE)
+  write.csv(x = SPQV_results, file = "paper_figures/output_data/SPQV_results.csv", row.names=FALSE)
 
 }
 
@@ -101,8 +104,8 @@ if (regenerate_results) {
 # 7  ye_m | no_bb | unid | n_dup  6
 # 8  ye_m | no_bb | bidi | n_dup  8
 
-RWR_results <- read.csv("paper_figures/data/RWR_results.csv", stringsAsFactors = FALSE)
-SPQV_results <- read.csv("paper_figures/data/SPQV_results.csv", stringsAsFactors = FALSE)
+RWR_results <- read.csv("paper_figures/output_data/RWR_results.csv", stringsAsFactors = FALSE)
+SPQV_results <- read.csv("paper_figures/output_data/SPQV_results.csv", stringsAsFactors = FALSE)
 qtl_range_to_show <- 1:199
 RWR_trunc <- RWR_results[c(1, 3, 2, 4, 5, 7, 6, 8), qtl_range_to_show]
 SPQV_trunc <- SPQV_results[qtl_range_to_show,]
@@ -120,6 +123,7 @@ pct_change_SR <- RWR_trunc
 pct_diff_avg <- RWR_trunc
 pct_diff_max <- RWR_trunc
 pct_diff_min <- RWR_trunc
+subtract_diff <- RWR_trunc
 ceiling_diff <- RWR_trunc
 for (row_i in 1:nrow(method_ratios)) {
   rwr <- RWR_trunc[row_i, ]
@@ -147,7 +151,9 @@ for (row_i in 1:nrow(method_ratios)) {
     (spqv - rwr) /
     ((spqv + rwr)/2)
 
-  # THIS IS THE ONE WE'RE USING
+  subtract_diff[row_i, ] <-
+    (spqv - rwr)
+
   ceiling_diff[row_i, ] <-
     (ceiling(spqv) - ceiling(unlist(rwr)))
 }
@@ -173,12 +179,17 @@ showHeatmap(
 
 # Ceiling plot ####
 bin_width <- 1
-max_val <- 10
+max_val <- 5
 color_breaks <- seq(bin_width, max_val, by = bin_width)
 color_breaks <- sort(c(color_breaks, -color_breaks, -0.01, 0.01))
 showHeatmap(
   ceiling_diff,
   color_breaks = color_breaks
+)
+
+showHeatmap(
+  subtract_diff,
+  num_colors = 41
 )
 
 
